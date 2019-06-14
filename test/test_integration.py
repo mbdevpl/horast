313ce43563553e0ab51d1ad64ee70b956ace102d
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2019-06-14 22:44:22 +0900

    test unparsing comments directly

--- test/test_integration.py
+++ test/test_integration.py
@@ -5,13 +5,27 @@ import unittest
 
 import typed_ast.ast3
 
-from horast.nodes import Directive
+from horast.nodes import Comment, Directive
 from horast.unparser import unparse
 
 _LOG = logging.getLogger(__name__)
 
 
 class Tests(unittest.TestCase):
+
+    def test_unparse_comment(self):
+        tree = typed_ast.ast3.If(
+            typed_ast.ast3.NameConstant(True),
+            [typed_ast.ast3.Pass(), Comment(typed_ast.ast3.Str(' hello world', ''), eol=False)], [])
+        code = unparse(tree)
+        self.assertEqual(code.strip(), 'if True:\n    pass\n    # hello world')
+
+    def test_unparse_eol_comment(self):
+        tree = typed_ast.ast3.If(
+            typed_ast.ast3.NameConstant(True),
+            [typed_ast.ast3.Pass(), Comment(typed_ast.ast3.Str(' hello world', ''), eol=True)], [])
+        code = unparse(tree)
+        self.assertEqual(code.strip(), 'if True:\n    pass  # hello world')
 
     def test_unparse_directive(self):
         tree = typed_ast.ast3.If(
