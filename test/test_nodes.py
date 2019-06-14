ef98666a5a039308fafec6e7848253aaaf9478c7
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2019-06-14 22:00:38 +0900

    test Comment node initialization

--- test/test_nodes.py
+++ test/test_nodes.py
@@ -12,10 +12,19 @@ _LOG = logging.getLogger(__name__)
 
 class Tests(unittest.TestCase):
 
-    @unittest.skip('eol field is temporarily disabled')
     def test_comment(self):
         expr_fields = typed_ast.ast3.Expr._fields
         comment_fields = Comment._fields
         _LOG.warning('%s', expr_fields)
         _LOG.warning('%s', comment_fields)
         self.assertGreater(len(comment_fields), len(expr_fields))
+
+        comment = Comment(typed_ast.ast3.Str(' comment', ''), False)
+        self.assertFalse(comment.eol)
+
+        eol_comment = Comment(typed_ast.ast3.Str(' comment', ''), True)
+        self.assertTrue(eol_comment.eol)
+
+        default_comment = Comment(typed_ast.ast3.Str(' comment', ''))
+        with self.assertRaises(AttributeError):
+            default_comment.eol
